7e9ee0be9230c86454d71d09d5c05cc47fddd2e5
biotest05real <169047576+biotest05real@users.noreply.github.com>
2024-06-25 11:45:07 +0200

    updated the location to be above the motd.

--- GorillaShirts/Locations/Forest.cs
+++ GorillaShirts/Locations/Forest.cs
@@ -6,6 +6,6 @@ namespace GorillaShirts.Locations
     internal class Forest : IStandLocation
     {
         public GTZone Zone => GTZone.forest;
-        public Tuple<UnityEngine.Vector3, UnityEngine.Vector3> Location => Tuple.Create<UnityEngine.Vector3, UnityEngine.Vector3>(new(-67.6651f, 12.07f, -80.438f), new(0f, 171.1801f, 0f));
+        public Tuple<UnityEngine.Vector3, UnityEngine.Vector3> Location => Tuple.Create<UnityEngine.Vector3, UnityEngine.Vector3>(new(-65.3888f, 12.05f, -84.3397f), new(0f, 317.6803f, 0f));
     }
 }
