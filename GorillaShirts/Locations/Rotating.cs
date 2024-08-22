986ed0c38787a538cf367d29c39349e100e75153
biotest05 <169047576+biotest05real@users.noreply.github.com>
2024-08-22 11:50:33 +0200

    Fixed the rotating stand.
    Made it on the table instead of being in the air

--- GorillaShirts/Locations/Rotating.cs
+++ GorillaShirts/Locations/Rotating.cs
@@ -6,6 +6,6 @@ namespace GorillaShirts.Locations
     internal class Rotating : IStandLocation
     {
         public GTZone Zone => GTZone.rotating;
-        public Tuple<UnityEngine.Vector3, UnityEngine.Vector3> Location => Tuple.Create<UnityEngine.Vector3, UnityEngine.Vector3>(new(-72.8298f, -74.271f, -134.549f), new(0f, 301.6311f, 0f));
+        public Tuple<UnityEngine.Vector3, UnityEngine.Vector3> Location => Tuple.Create<UnityEngine.Vector3, UnityEngine.Vector3>(new(-68.4334f, -73.54f, -131.5085f), new(0f, 355.1183f, 0f));
     }
 }
