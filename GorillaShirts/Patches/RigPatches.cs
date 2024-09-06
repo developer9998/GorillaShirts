07effa874f1fa552d46c55e7d732a6bda2fa3a38
dev <81720436+developer9998@users.noreply.github.com>
2024-09-06 18:42:36 -0230

    Update RigPatches.cs

--- GorillaShirts/Patches/RigPatches.cs
+++ GorillaShirts/Patches/RigPatches.cs
@@ -6,8 +6,8 @@ namespace GorillaShirts.Patches
     [HarmonyPatch]
     public class RigPatches
     {
-        public static void AddPatch(NetPlayer player, VRRig vrrig) => Events.RigEnabled?.Invoke(player is PunNetPlayer punPlayer ? punPlayer.playerRef : null, vrrig);
+        public static void AddPatch(NetPlayer player, VRRig vrrig) => Events.RigEnabled?.Invoke(player is PunNetPlayer punPlayer ? punPlayer.PlayerRef : null, vrrig);
 
-        public static void RemovePatch(NetPlayer player, VRRig vrrig) => Events.RigDisabled?.Invoke(player is PunNetPlayer punPlayer ? punPlayer.playerRef : null, vrrig);
+        public static void RemovePatch(NetPlayer player, VRRig vrrig) => Events.RigDisabled?.Invoke(player is PunNetPlayer punPlayer ? punPlayer.PlayerRef : null, vrrig);
     }
 }
