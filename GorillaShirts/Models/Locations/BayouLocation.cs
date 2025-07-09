a19791934110eefe8d8e603aeab4f90569d9b811
Golden <goldenisaprotogen@tutamail.com>
2025-07-08 20:36:33 -0400

    Update BayouLocation.cs

--- GorillaShirts/Models/Locations/BayouLocation.cs
+++ GorillaShirts/Models/Locations/BayouLocation.cs
@@ -5,7 +5,7 @@ namespace GorillaShirts.Models.Locations
     public class BayouLocation : IStandLocation
     {
         public GTZone[] Zones => [GTZone.bayou];
-        public Vector3 Position => new(-123.1133f, -12.1695f, -91.0308f);
+        public Vector3 Position => new(-147.6207f, -15.2281f, -64.5603f);
         public Vector3 EulerAngles => Vector3.up * 34.6322f;
     }
 }
