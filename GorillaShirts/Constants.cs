d561c332d71fac29bf3af7c3bbc4fe79c35974fa
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2025-08-04 22:31:33 -0230

    minor updates

--- GorillaShirts/Constants.cs
+++ GorillaShirts/Constants.cs
@@ -6,7 +6,7 @@
 
         public const string Name = "GorillaShirts";
 
-        public const string Version = "2.2.0";
+        public const string Version = "2.2.1";
 
         public const float NetworkRaiseInterval = 0.25f;
         public const string NetworkPropertyKey = "GorillaShirts";
