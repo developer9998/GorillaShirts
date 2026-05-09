7d6d7b5a60211475f08bb7992aabc703a413baab
Dane <81720436+developer9998@users.noreply.github.com>
2026-05-09 13:52:13 -0230

    add dependency attribute

--- GorillaShirts/Plugin.cs
+++ GorillaShirts/Plugin.cs
@@ -12,53 +12,53 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-namespace GorillaShirts
+namespace GorillaShirts;
+
+[BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+[BepInDependency("dev.gorillalibrary")]
+internal class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
-    internal class Plugin : BaseUnityPlugin
+    public static bool State;
+
+    public static event Action<bool> OnStateChanged;
+
+    public static new PluginInfo Info;
+
+    public static new ManualLogSource Logger;
+
+    public static ConfigEntry<CharacterPreference> StandCharacter;
+
+    public static ConfigEntry<string> Favourites;
+
+    public static ConfigEntry<EDefaultShirtMode> DefaultShirtMode;
+
+    public void Awake()
     {
-        public static bool State;
+        Info = base.Info;
+        Logger = base.Logger;
 
-        public static event Action<bool> OnStateChanged;
+        Config.SaveOnConfigSet = true;
 
-        public static new PluginInfo Info;
+        Favourites = Config.Bind("Preferences", "Favourites", JsonConvert.SerializeObject(Enumerable.Empty<string>()), "A collection of shirts favourited by the player");
 
-        public static new ManualLogSource Logger;
+        var characters = Enum.GetValues(typeof(CharacterPreference)).Cast<CharacterPreference>().ToArray();
+        StandCharacter = Config.Bind("Appearance", "Stand Character Identity", characters[UnityEngine.Random.Range(0, characters.Length)], "The gender identity of the character present at the shirt stand");
 
-        public static ConfigEntry<CharacterPreference> StandCharacter;
+        DefaultShirtMode = Config.Bind("Appearance", "Default Shirt Mode", EDefaultShirtMode.None, "The method used for how shirts are worn by players without the mod, known as default shirts");
 
-        public static ConfigEntry<string> Favourites;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Constants.GUID);
+        GorillaTagger.OnPlayerSpawned(() => DontDestroyOnLoad(new GameObject($"{Constants.Name} {Constants.Version}", typeof(NetworkSolution_RaiseEvent), typeof(DataManager), typeof(ShirtManager))));
+    }
 
-        public static ConfigEntry<EDefaultShirtMode> DefaultShirtMode;
+    public void OnEnable()
+    {
+        State = true;
+        OnStateChanged?.Invoke(true);
+    }
 
-        public void Awake()
-        {
-            Info = base.Info;
-            Logger = base.Logger;
-
-            Config.SaveOnConfigSet = true;
-
-            Favourites = Config.Bind("Preferences", "Favourites", JsonConvert.SerializeObject(Enumerable.Empty<string>()), "A collection of shirts favourited by the player");
-
-            var characters = Enum.GetValues(typeof(CharacterPreference)).Cast<CharacterPreference>().ToArray();
-            StandCharacter = Config.Bind("Appearance", "Stand Character Identity", characters[UnityEngine.Random.Range(0, characters.Length)], "The gender identity of the character present at the shirt stand");
-
-            DefaultShirtMode = Config.Bind("Appearance", "Default Shirt Mode", EDefaultShirtMode.None, "The method used for how shirts are worn by players without the mod, known as default shirts");
-
-            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Constants.GUID);
-            GorillaTagger.OnPlayerSpawned(() => DontDestroyOnLoad(new GameObject($"{Constants.Name} {Constants.Version}", typeof(NetworkSolution_RaiseEvent), typeof(DataManager), typeof(ShirtManager))));
-        }
-
-        public void OnEnable()
-        {
-            State = true;
-            OnStateChanged?.Invoke(true);
-        }
-
-        public void OnDisable()
-        {
-            State = false;
-            OnStateChanged?.Invoke(false);
-        }
+    public void OnDisable()
+    {
+        State = false;
+        OnStateChanged?.Invoke(false);
     }
 }
